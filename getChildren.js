23a9fc0ddcae0f73fc29465579f342c49283b74a
wenogk <drf325@nyu.edu>
2020-10-07 09:45:28 +0400

    updates and color coding

--- getChildren.js
+++ getChildren.js
@@ -1,39 +1,105 @@
 function Check(o) {
   return o instanceof Element;
 }
+function addCss() {
+  var link = document.createElement("link");
+  link.href = chrome.extension.getURL("injectionCSS.css");
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  document.getElementsByTagName("head")[0].appendChild(link);
+}
+function getBoundingRect(elem, ifErrorReturnZeros = false) {
+  if (Check(elem)) {
+    let boundingRectNode = elem.getBoundingClientRect();
+    cx = boundingRectNode.left;
+    cy = boundingRectNode.top;
+    cw = boundingRectNode.width;
+    ch = boundingRectNode.height;
+
+    return {
+      x: cx,
+      y: cy,
+      w: cw,
+      h: ch,
+    };
+  } else {
+    if (ifErrorReturnZeros) {
+      return {
+        x: 0,
+        y: 0,
+        w: 0,
+        h: 0,
+      };
+    }
+  }
+}
+
+function colorBasedOnType(elem) {
+  interactiveElements = ["A", "BUTTON"];
+  if (interactiveElements.includes(elem.nodeName)) {
+    if (elem.nodeName == "A") {
+      if (elem.href) {
+        return "red";
+      } else {
+        return "blue";
+      }
+    }
+    return "red";
+  } else {
+    return "#4B0082";
+  }
+}
+
 function getDef(element, def) {
+  //addCss();
   var str = "";
 
   var childs = element.childNodes;
 
-  let tempRowTop = 0;
-  let tempColLeftmost = 0;
-
+  let tempRowTop = getBoundingRect(element, true).y;
+  let tempColLeftmost = getBoundingRect(element, true).x;
+  //find first valid child and set top and left vals
+  /*
+  let index = 0;
+  let skipDefault = false;
+  while (!Check(childs[index])) {
+    index += 1;
+    if (index >= childs.length) {
+      skipDefault = true;
+      break;
+    }
+  }
+  if (!skipDefault) {
+    tempRowTop = childs[index].getBoundingClientRect().top;
+    tempColLeftmost = childs[index].getBoundingClientRect().left;
+  }
+*/
   for (var i = 0; i < childs.length; ++i) {
-    if (childs[i].nodeType != 3) {
-      let typeOfChild = "";
+    if (childs[i].nodeType != 3 && childs[i].nodeType != 8) {
+      let typeOfChild = [];
       if (Check(childs[i]) && childs[i].getBoundingClientRect().width == 0) {
         continue;
       } else if (Check(childs[i])) {
-        let boundingRectNode = childs[i].getBoundingClientRect();
-        cx = boundingRectNode.left;
-        cy = boundingRectNode.top;
-        cw = boundingRectNode.width;
-        ch = boundingRectNode.height;
-
-        if (cy > tempRowTop) {
-          typeOfChild += "row_split";
+        if (getBoundingRect(childs[i]).y > tempRowTop) {
+          typeOfChild.push("row_split");
           tempRowTop = cx;
         }
 
-        if (cx > tempColLeftmost) {
-          typeOfChild += "col_split";
+        if (getBoundingRect(childs[i]).x > tempColLeftmost) {
+          typeOfChild.push("col_split");
           tempColLeftmost = cx;
         }
       }
       let left = parseInt(def) * 10;
       str +=
-        "  ".repeat(def) + childs[i].nodeName + " - " + typeOfChild + "<br />";
+        "  ".repeat(def) +
+        "<font color='" +
+        colorBasedOnType(childs[i]) +
+        "'>" +
+        childs[i].nodeName +
+        "</font> - " +
+        typeOfChild.join(", ") +
+        "<br />";
       str += getDef(childs[i], def + 1);
     }
   }
